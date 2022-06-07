e3aff3f4e162bd68726907c44d0b12229eb7252f
Deyan <gidikovdev@gmail.com>
2022-06-07 18:15:41 +0300

    added additional projects
    added folder arrays-exercises to main folder c# fund.

---
+++ SoftUni/C# Fundamentals/Arrays - Exercise/08. Magic Sum/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08.Magic_Sum
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] randomNumbers= Console.ReadLine().Split().Select(int.Parse).ToArray();
+			int randomNum = int.Parse(Console.ReadLine());
+
+			for (int i = 0; i < randomNumbers.Length; i++)
+			{
+				int currentNum = randomNumbers[i];
+
+				for (int j = i + 1; j < randomNumbers.Length; j++) 
+				{
+					int secondCurrent = randomNumbers[j];
+
+					if (currentNum + secondCurrent == randomNum)
+					{
+						Console.Write($"{currentNum} {secondCurrent}");
+						Console.WriteLine();
+						break;
+					}
+				}
+			}
+		}
+	}
+}
