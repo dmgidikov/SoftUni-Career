a9a2f7ef7e4ab0d84d33095d76cbaa71711c257b
Deyan <gidikovdev@gmail.com>
2022-06-26 12:01:46 +0300

    added additional folder Lists-Lab
    Added additional folder List-Lab with lab exercises training skills with using Lists

---
+++ SoftUni/C# Fundamentals/Lists - Lab/03. Merging Lists/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Merging_Lists
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			List<int> firstNUms = Console.ReadLine().Split().Select(int.Parse).ToList();
+			List<int> secondNums = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+			int firsNumsCount = firstNUms.Count();
+			int secondNumsCount = secondNums.Count();
+
+			int shorterCount = Math.Min(firsNumsCount, secondNumsCount);
+			
+
+			List<int> result = new List<int>();
+
+			for (int i = 0; i < shorterCount; i++)
+			{
+				result.Add(firstNUms[i]);
+				result.Add(secondNums[i]);
+			}
+
+			if (firsNumsCount != secondNumsCount)
+			{
+				if (firsNumsCount > secondNumsCount)
+				{
+					for (int i = secondNumsCount; i < firsNumsCount; i++)
+					{
+						result.Add(firstNUms[i]);
+					}
+				}
+				else
+				{
+					for (int i = firsNumsCount; i < secondNumsCount; i++)
+					{
+						result.Add(secondNums[i]);
+					}
+				}
+			}
+
+			Console.WriteLine(String.Join(" ", result));
+		}
+	}
+}
---
+++ SoftUni/C# Fundamentals/Lists - Lab/06. List Manipulation Basics/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.List_Manipulation_Basics
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			List<int> randomNums = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+			while (true)
+			{
+				string[] command = Console.ReadLine().Split();
+
+				if (command[0] == "end")
+				{
+					Console.WriteLine(String.Join(" ", randomNums));
+					break;
+				}
+				else
+				{
+					if (command[0] == "Add")
+					{
+						randomNums.Add(GetNum(command));
+					}
+					else if (command[0] == "Remove")
+					{
+						randomNums.Remove(GetNum(command));
+					}
+					else if (command[0] == "RemoveAt")
+					{
+						randomNums.RemoveAt(GetNum(command));
+					}
+					else if (command[0] == "Insert")
+					{
+						int index = int.Parse(command[2]);
+						randomNums.Insert(index, GetNum(command));
+					}
+				}
+			}
+		}
+
+		static int GetNum(string[] command)
+		{
+			int number = int.Parse(command[1]);
+			return number;
+		}
+	}
+}
