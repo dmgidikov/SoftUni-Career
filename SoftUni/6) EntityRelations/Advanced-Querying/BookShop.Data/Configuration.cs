1b0d69d8c933320f92410e4a6706018982f25d10
Deyan Gidikov <gidikovdev@gmail.com>
2023-11-30 19:32:12 +0200

    Advanced-Quarying
    Add additional folder to the course with passed task for advanced quarying

---
+++ SoftUni/6) EntityRelations/Advanced-Querying/BookShop.Data/Configuration.cs
@@ -0,0 +1,9 @@
+﻿namespace BookShop.Data
+{
+    internal class Configuration
+    {
+        internal static string ConnectionString
+            => "Server=DESKTOP-KMLVDNC\\SQLEXPRESS;Database=BookShop;Trusted_Connection=SSPI;Encrypt=false;TrustServerCertificate=true";
+
+    }
+}
