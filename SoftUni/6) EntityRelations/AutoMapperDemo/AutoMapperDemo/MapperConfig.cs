79f5a5cb5b18af1a0d181f79716225cc556a03f7
Deyan Gidikov <gidikovdev@gmail.com>
2024-01-17 09:25:53 +0200

    Edit EntityRelations
    Add auto-mapper

---
+++ SoftUni/6) EntityRelations/AutoMapperDemo/AutoMapperDemo/MapperConfig.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoMapperDemo
+{
+    public class MapperConfig
+    {
+        public static Mapper InitializeAutomapper()
+        {
+            var confing = new MapperConfiguration(cfg =>
+            cfg.CreateMap<Employee, EmployeeDTO>()
+            .ForMember(x => x.FullName, y => y.MapFrom(x => x.Name))
+            .ForMember(x => x.Dept, y => y.MapFrom(x => x.Department)));
+
+            var mapper = new Mapper(confing);
+
+            return mapper;
+        }
+    }
+}
