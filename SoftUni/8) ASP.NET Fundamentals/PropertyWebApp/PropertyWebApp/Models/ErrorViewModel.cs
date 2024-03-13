8c6b07109bfb12c9cd1584d131acc344b1c5b736
Deyan Gidikov <gidikovdev@gmail.com>
2024-03-13 18:50:15 +0200

    SoftUni Update
    delete wrong location files

--- SoftUni/8) ASP.NET Fundamentals/PropertyWebApp/PropertyWebApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PropertyWebApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
